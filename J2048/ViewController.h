825a48ff81cab878e4f9348233d9e179b75c0d6c
Jujuxw <13389634266@189.cn>
2016-12-16 21:30:39 +0800

    Initial Commit

---
+++ J2048/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  J2048
+//
+//  Created by juju on 2016/12/16.
+//  Copyright © 2016年 juju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ J2048/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  J2048
+//
+//  Created by juju on 2016/12/16.
+//  Copyright © 2016年 juju. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
